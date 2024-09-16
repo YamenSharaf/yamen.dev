97003f238518edc7200a067c288fe3956797f155
Yamen Sharaf <yamensharaf@gmail.com>
2024-09-16 23:44:08 +0200

    config without pages

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,21 +1,15 @@
 // stackbit.config.ts
 import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
+console.log('xyz __dirname:', __dirname)
 
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
   contentSources: [
     new GitContentSource({
-      rootPath: `${__dirname}/src`,
+      rootPath: `${__dirname}/src/`,
       contentDirs: ["content"],
       models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "content/pages/{slug}.json",
-          fields: [{ name: "title", type: "string", required: true }]
-        },
         {
           name: "Post",
           type: "page",
