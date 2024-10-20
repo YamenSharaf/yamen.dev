629af2e3605ba8a10f64b0c661c0f107363d0336
Yamen Sharaf <yamensharaf@gmail.com>
2024-10-20 16:00:51 +0200

    Updates Blog Alert Component

--- src/components/blog/BlogAlert.tsx
+++ src/components/blog/BlogAlert.tsx
@@ -1,12 +1,12 @@
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
-interface AlertDemoProps {
+interface BlogAlertProps {
   children: React.ReactNode;
-  title: string;
+  title?: string;
 }
 
-export function AlertDemo({ children, title }: AlertDemoProps) {
+export function BlogAlert({ children, title = "Heads up!" }: BlogAlertProps) {
   return (
     <Alert variant="destructive">
       <div className="items-center">
