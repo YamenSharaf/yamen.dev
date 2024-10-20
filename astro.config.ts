b248c31dd9f363ac1ddf456bfee88051e764ca7e
Yamen Sharaf <yamensharaf@gmail.com>
2024-10-20 13:57:45 +0200

    Adds MDX support

--- astro.config.ts
+++ astro.config.ts
@@ -6,16 +6,14 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), sitemap(), mdx()],
   markdown: {
     remarkPlugins: [
       remarkToc,
